6ce6a4be9cac1b853703020b60baf7a515899f0a
S1grud <66888225+S1grud@users.noreply.github.com>
2025-07-19 20:14:13 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
+
+# Запускаем Lavalink в фоне
 java -jar /opt/Lavalink/Lavalink.jar &
 
+# Ждем несколько секунд, чтобы Lavalink стартовал
+sleep 10
+
+# Запускаем Discord-бота (Python)
 exec python3 /app/bot.py
